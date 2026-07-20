52e81bcdd80cf58475e7f09a42e948aa452296f8
gerald-gv <geraldgutierrez918@gmail.com>
2026-07-20 17:22:22 -0500

    firts commit StyleMax

---
+++ src/test/java/com/stylemax/stylemax_api/StylemaxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylemax.stylemax_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylemaxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
