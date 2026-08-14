e399a52d13a75c5f3b7296aa2b16f474ef06415c
gerald-gv <geraldgutierrez918@gmail.com>
2026-08-14 01:10:58 -0500

    feat: add Docker configuration

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# COMPILE STAGE
+FROM maven:3.9-eclipse-temurin-21-alpine AS build
+
+WORKDIR /app
+
+COPY pom.xml .
+
+RUN mvn dependency:go-offline -B
+
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+
+# RUNTIME STAGE
+FROM eclipse-temurin:21-jre
+
+WORKDIR /app
+
+COPY --from=build /app/target/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/stylemax/stylemax_api/Config/CorsConfig.java
+++ src/main/java/com/stylemax/stylemax_api/Config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.stylemax.stylemax_api.Config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,12 +11,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
-
+	
+	@Value("${app.frontend-url}")
+    private String frontendUrl;
+	
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(frontendUrl));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
