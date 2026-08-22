c9f2726816b5c7147db5d42b07747af0c211aea0
Gerald Gutierrez <geraldgutierrez918@gmail.com>
2026-08-21 19:55:43 -0500

    Fix request matchers for health endpoint

--- src/main/java/com/stylemax/stylemax_api/Config/SecurityConfig.java
+++ src/main/java/com/stylemax/stylemax_api/Config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                         .requestMatchers(HttpMethod.GET, "/health")
+                         .requestMatchers("/health")
                             .permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/api/productos/**",
